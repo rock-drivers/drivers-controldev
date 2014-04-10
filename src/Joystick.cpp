55fbb6846084351616bd13dbce2b1a81dc5ca2ad
Javier Hidalgo Carrio <javier.hidalgo_carrio@dfki.de>
2014-04-10 11:46:01 +0200

    Added joystick EvdevHelper classes from the jstest-gtk package Adapt the classes for the controdev library to work with this helper function

--- src/Joystick.cpp
+++ src/Joystick.cpp
@@ -1,5 +1,6 @@
 
 #include "Joystick.hpp"
+#include "EvdevHelper.hpp"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -8,83 +9,151 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <linux/joystick.h>
-#include <errno.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <sstream>
+#include <stdexcept>
+#include <limits>
+
 
 namespace controldev
 {
 
-    Joystick::Joystick() : initialized(false), deadspot(false), deadspot_size(0) {
+    Joystick::Joystick() : initialized(false), deadspot(false), deadspot_size(0)
+    {
 	axes = 0;
 	buttons = 0;
 	fd = -1;
     }
 
-    bool Joystick::init(std::string const& dev) {
-      initialized = false;
-
-      if(axes) {
+    Joystick::~Joystick()
+    {
+      if(axes)
 	delete[] axes;
-	axes = 0;
-      }
-      
-      if(buttons) {
+
+      if(buttons)
 	delete[] buttons;
-	buttons = 0;
-      }
-      
+
       if(fd != -1)
 	close(fd);
-
-      if ((fd = open(dev.c_str(),O_RDONLY | O_NONBLOCK)) < 0) {
-        //std::cout << "Warning: could not initialize joystick.\n";
-	fd = -1;
-        return false;
-      } 
-
-      if(ioctl(fd, JSIOCGAXES, &nb_axes) == -1) {
-        perror("axes");
-        return false;
-      }
-      if(ioctl(fd, JSIOCGBUTTONS, &nb_buttons) == -1) {
-        perror("button");
-        return false;	
-      }
-      
-      char name_char[50];
-      
-      if(ioctl(fd, JSIOCGNAME(50), name_char) == -1) {
-        perror("name");
-        return false;	
-      }
-      
-      name = std::string(name_char);
-
-      std::cout << "Axes: " << (int) nb_axes <<" Buttons: " << (int) nb_buttons << " Name: " << name <<std::endl;
-
-      axes = new int[nb_axes];
-      buttons = new int[nb_buttons];
-      
-      for(int i = 0; i < nb_buttons; i++) {
-        buttons[i] = 0;
-      }
-
-      for(int i = 0; i < nb_axes; i++) {
-        axes[i] = 0;
-      }
-
-      initialized = true;
-
-      return true;
+	
+      if(!initialized)
+        return;
     }
 
-      
+    bool Joystick::init(std::string const& dev)
+    {
+        initialized = false;
+
+        if(axes) {
+        delete[] axes;
+        axes = 0;
+        }
+
+        if(buttons) {
+        delete[] buttons;
+        buttons = 0;
+        }
+
+        if(fd != -1)
+        close(fd);
+
+        if ((fd = open(dev.c_str(),O_RDONLY | O_NONBLOCK)) < 0) {
+        //std::cout << "Warning: could not initialize joystick.\n";
+        fd = -1;
+        return false;
+        } 
+
+        if(ioctl(fd, JSIOCGAXES, &nb_axes) == -1) {
+        perror("axes");
+        return false;
+        }
+        if(ioctl(fd, JSIOCGBUTTONS, &nb_buttons) == -1) {
+        perror("button");
+        return false;	
+        }
+
+        char name_char[50];
+
+        if(ioctl(fd, JSIOCGNAME(50), name_char) == -1) {
+        perror("name");
+        return false;	
+        }
+
+
+        /** Get the axis mapping **/
+        uint8_t axismap[ABS_MAX + 1];
+        if (ioctl(fd, JSIOCGAXMAP, axismap) < 0)
+        {
+            std::ostringstream str;
+            str << dev << ": " << strerror(errno);
+            return false;
+        }
+        else
+        {
+            std::copy(axismap, axismap + nb_axes, std::back_inserter(this->axis_mapping));
+        }
+
+        /** Get the button mapping **/
+        uint16_t btnmap[KEY_MAX - BTN_MISC + 1];
+        if (ioctl(fd, JSIOCGBTNMAP, btnmap) < 0)
+        {
+            std::ostringstream str;
+            str << dev << ": " << strerror(errno);
+            return false;
+        }
+        else
+        {
+            std::copy(btnmap, btnmap + nb_buttons, std::back_inserter(this->button_mapping));
+        }
+
+        name = std::string(name_char);
+
+        axes = new int[nb_axes];
+        buttons = new int[nb_buttons];
+
+        for(int i = 0; i < nb_buttons; i++) {
+        buttons[i] = 0;
+        }
+
+        for(int i = 0; i < nb_axes; i++) {
+        axes[i] = 0;
+        }
+
+        /** Generate and Display Joystick information **/
+        std::cout<< "Joystick ("<< name <<") has "<< (int) nb_axes <<" Axes (";
+        for(std::vector<int>::iterator i=this->axis_mapping.begin(); i!=this->axis_mapping.end(); ++i)
+        {
+            this->axis_names.push_back(abs2str(*i));
+            std::cout<<abs2str(*i)<<" ";
+        }
+        std::cout<<")\n";
+
+        std::cout << "It has "<< (int) nb_buttons << " Buttons (";
+        for(std::vector<int>::iterator i=this->button_mapping.begin(); i!=this->button_mapping.end(); ++i)
+        {
+            this->button_names.push_back(btn2str(*i));
+            std::cout<<btn2str(*i)<<" ";
+        }
+        std::cout<<")\n";
+        /**  End Print **/
+
+
+        initialized = true;
+
+        return true;
+    }
+
+
     void Joystick::setDeadspot(bool onOff, double size) {
       deadspot = onOff;
       deadspot_size = size;
     }
 
-    bool Joystick::updateState() {
+    bool Joystick::updateState()
+    {
       struct js_event mybuffer[64];
       int n, i;
 
@@ -99,7 +168,7 @@ namespace controldev
           }
           else if(mybuffer[i].type & JS_EVENT_AXIS &~ JS_EVENT_INIT) {
         axes[mybuffer[i].number] = mybuffer[i].value;
-        
+
         if(mybuffer[i].number == 0 || mybuffer[i].number == 1) {
           if(deadspot) {
             if(abs(axes[mybuffer[i].number]) < deadspot_size * 32767)
@@ -137,35 +206,44 @@ namespace controldev
 
     }
 
-    bool Joystick::getButtonPressed(int btn_nr) const{
+    bool Joystick::getButtonPressed(int btn_nr) const
+    {
       if(btn_nr > nb_buttons)
         return false;
-      
+
       return buttons[btn_nr];
     }
 
 
-
-    Joystick::~Joystick() {
-      if(axes)
-	delete[] axes;
-      
-      if(buttons)
-	delete[] buttons;
-
-      if(fd != -1)
-	close(fd);
-	
-      if(!initialized)
-        return;
+    std::vector<bool> Joystick::getButtons() const
+    {
+      std::vector<bool> button_values;
+      for (register int i=0; i<this->nb_buttons; ++i)
+      {
+          button_values.push_back(static_cast<bool>(buttons[i]));
+      }
+      return button_values;
     }
 
-    double Joystick::getAxis(Axis axis_nr) const
+
+    double Joystick::getAxis(int axis_nr) const
     {
-      if (!initialized) return 0;
-      if (axis_nr > nb_axes) return 0;
+      if (!initialized) return std::numeric_limits<double>::quiet_NaN();
+      if (axis_nr > nb_axes) return std::numeric_limits<double>::quiet_NaN();
 
       return axes[axis_nr] / 32767.0;
     }
+
+    std::vector<double> Joystick::getAxes() const
+    {
+      std::vector<double> axis_values;
+      if (!initialized) return axis_values;
+
+      for (register int i=0; i<this->nb_axes; ++i)
+          axis_values.push_back(static_cast<double>(axes[i]/32767.0));
+
+      return axis_values;
+    }
+
 
 }
--- src/Joystick.hpp
+++ src/Joystick.hpp
@@ -2,10 +2,13 @@
 #define JOYSTICK_HPP
 
 #include <string>
+#include <vector>
+#include <stdint.h>
 
 namespace controldev
 {
-    class Joystick {
+    class Joystick
+    {
       public:
       Joystick();
       virtual ~Joystick();
@@ -33,46 +36,61 @@ namespace controldev
 //      static const int SIDEWARD_AXIS = 0;
 //      static const int PAN_AXIS = 4;
 //      static const int TILT_AXIS = 5;
+//
+        virtual bool init(std::string const& dev);
 
-      virtual bool init(std::string const& dev);
-      
-      void setDeadspot(bool onOff, double size);
-      
-      virtual bool updateState();
-      
-      virtual double getAxis(Axis axis_nr) const;
+        void setDeadspot(bool onOff, double size);
 
-      virtual bool getButtonPressed(int btn_nr) const;
+        virtual bool updateState();
 
-      virtual int getNrAxis() const {
+        virtual double getAxis(int axis_nr) const;
+        std::vector<double> getAxes() const;
+
+        virtual bool getButtonPressed(int btn_nr) const;
+        virtual std::vector<bool> getButtons() const;
+
+        virtual int getNrAxis() const {
         return nb_axes;
-      }
-      
-      virtual int getNrButtons() const {
-        return nb_buttons;
-      }
+        }
 
-      int getFileDescriptor()
-      {
+        virtual int getNrButtons() const {
+        return nb_buttons;
+        }
+
+        int getFileDescriptor()
+        {
           return fd;
-      }
-    
-      std::string const getName() const {
-	  return name;
-      }
-      
+        }
+
+        std::string const getName() const {
+          return name;
+        }
+
+        std::vector<std::string> const getMapAxis() const
+        {
+            return this->axis_names;
+        }
+
+        std::vector<std::string> const getMapButtons() const
+        {
+            return this->button_names;
+        }
+
       private:
-      int fd;
-      bool initialized;
-      bool deadspot;
-      double deadspot_size;
-      
-      int *axes;
-      int *buttons;
-      unsigned char nb_buttons;
-      unsigned char nb_axes;
-      std::string name;
-        
+        int fd;
+        bool initialized;
+        bool deadspot;
+        double deadspot_size;
+
+        int *axes;
+        int *buttons;
+        unsigned char nb_buttons;
+        unsigned char nb_axes;
+        std::string name;
+        std::vector<int> axis_mapping;
+        std::vector<int> button_mapping;
+        std::vector<std::string> axis_names;
+        std::vector<std::string> button_names;
     };
-}     
+}
 #endif
