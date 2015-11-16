1417b02d6605b99330c6b1e877278756c375268c
Niklas Pech <niklas.pech@dfki.de>
2015-11-16 17:15:03 +0100

    include assert.h

--- src/GraupnerMC20.cpp
+++ src/GraupnerMC20.cpp
@@ -1,5 +1,6 @@
 #include "GraupnerMC20.hpp"
 #include <iostream>
+#include <assert.h>
 #define CRC_POLYNOME 0x1021
 
 using namespace controldev;
@@ -16,8 +17,7 @@ bool GraupnerMC20::getReading(MC20Output *out){
     int len = readPacket(buffer, buffersize, timeout);
     if(len == 37){
         for(int i = 0; i < buffer[2]; i++){
-            //std::cout << (int)buffer[i*2+1] << std::endl;
-            out->channel[i] = buffer[i*2+1]<<8 | buffer[i*2+2];
+           out->channel[i] = buffer[i*2+1]<<8 | buffer[i*2+2];
         }
         return true;
     } else {
@@ -36,34 +36,25 @@ int GraupnerMC20::extractPacket(uint8_t const* buffer, size_t buffer_size) const
         }
     }
     if(i>0){
-        //std::cout << "skipping " << i  << std::endl;
         return -i;
     }
     if(buffer[1] != 0x01 && buffer[1] != 0x81){
-        //std::cout << "2"  << std::endl;
         return -1;
     }
 
     if(buffer_size<3){
-        //std::cout << "3"  << std::endl;
         return 0;
     }
 
     int length = buffer[2]*2+5;
 
     if((int)buffer_size < length){
-        //std::cout << "Buffersize" << buffer_size << std::endl;
         return 0;
     }
 
     uint16_t crc = (buffer[(buffer[2]+1)*2+1]<<8) + buffer[(buffer[2]+1)*2+2];
-    //std::cout << "Im Packet:" << crc << std::endl;
-    //std::cout << "Gerechnet:" << genCRC(buffer) << std::endl;
 
     if (crc == genCRC(buffer)){
-        //std::cout << "4"  << std::endl;
-        //std::cout << "length:" << length << std::endl;
-        //std::cout << "buffer_size:" << buffer_size << std::endl;
         return length;
     }
         std::cout << "5"  << std::endl;
@@ -84,7 +75,6 @@ uint16_t GraupnerMC20::CRC(uint16_t crc, uint8_t value) const{
     uint8_t i;
 
     crc = crc ^ ((int16_t)value)<<8;
-    //std::cout << "VORNE: " << crc << std::endl;
 
     for(i=0; i<8; i++){
         if(crc & 0x8000){
