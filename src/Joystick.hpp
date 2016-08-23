ae71d6bfcdad937a88e6723711b0fbc11071ec01
Janosch Machowinski <janosch.machowinski@dfki.de>
2016-08-23 14:30:49 +0200

    modernized code base / repaired dead zone feature

--- src/Joystick.hpp
+++ src/Joystick.hpp
@@ -2,53 +2,64 @@
 #define JOYSTICK_HPP
 
 #include <string>
+#include <vector>
 
 namespace controldev
 {
-    class Joystick {
-      public:
-      Joystick();
-      virtual ~Joystick();
+class Joystick {
+public:
+    Joystick();
+    virtual ~Joystick();
 
-      virtual bool init(std::string const& dev);
-      
-      void setDeadspot(bool onOff, double size);
-      
-      virtual bool updateState();
-      
-      virtual double getAxis(size_t axis_nr) const;
-
-      virtual bool getButtonPressed(int btn_nr) const;
-
-      virtual int getNrAxis() const {
-        return nb_axes;
-      }
-      
-      virtual int getNrButtons() const {
-        return nb_buttons;
-      }
-
-      int getFileDescriptor()
-      {
-          return fd;
-      }
+    virtual bool init(std::string const& dev);
     
-      std::string const getName() const {
-	  return name;
-      }
-      
-      private:
-      int fd;
-      bool initialized;
-      bool deadspot;
-      double deadspot_size;
-      
-      int *axes;
-      int *buttons;
-      unsigned char nb_buttons;
-      unsigned char nb_axes;
-      std::string name;
-        
+    void setDeadspot(bool onOff, double size);
+    
+    virtual bool updateState();
+    
+    virtual double getAxis(size_t axis_nr) const;
+
+    virtual bool getButtonPressed(size_t btn_nr) const;
+
+    virtual size_t getNrAxis() const {
+    return axes.size();
+    }
+    
+    virtual size_t getNrButtons() const {
+    return buttons.size();
+    }
+
+    int getFileDescriptor()
+    {
+        return fd;
+    }
+
+    std::string const getName() const {
+        return name;
+    }
+    
+    const std::vector<double> &getAxes() const
+    {
+        return axes;
     };
+    const std::vector<bool> &getButtons() const
+    {
+        return buttons;
+    }
+    
+private:
+    
+    void applyDeadSpot();
+    
+    int fd;
+    bool initialized;
+    bool deadspot;
+    double deadspot_size;
+
+    std::vector<double> axes;
+    std::vector<bool> buttons;
+    std::string name;
+    
+};
 }     
 #endif
